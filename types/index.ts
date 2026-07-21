894b0478224e132e3aa04e7d391ebe9f6b8fc058
Name1 <nuxotetotmailsvoboden@gmail.com>
2026-07-22 01:01:30 +0500

    Update index.ts

--- types/index.ts
+++ types/index.ts
@@ -1,1 +1,12 @@
+export interface TelegramUser {
+  id: number;
+  first_name: string;
+  last_name?: string;
+  username?: string;
+  language_code?: string;
+}
 
+export interface UserState {
+  user: TelegramUser | null;
+  isAdmin: boolean;
+}
