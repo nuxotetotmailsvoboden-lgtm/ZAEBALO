674717584ed083cb5b2edac034527fd580b901a3
Name1 <nuxotetotmailsvoboden@gmail.com>
2026-07-22 01:14:29 +0500

    Create telegramBot.ts

---
+++ services/telegramBot.ts
@@ -0,0 +1,13 @@
+export async function sendTelegramNotification(token: string, chatId: string | number, text: string) {
+  const url = `https://api.telegram.org/bot${token}/sendMessage`;
+  const res = await fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      chat_id: chatId,
+      text,
+      parse_mode: 'HTML',
+    }),
+  });
+  return res.json();
+}
