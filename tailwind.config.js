11a73f313946e184ebf190931801c20ae6abe479
Name1 <nuxotetotmailsvoboden@gmail.com>
2026-07-22 15:32:22 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,16 @@ module.exports = {
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
         border: "hsl(var(--border))",
