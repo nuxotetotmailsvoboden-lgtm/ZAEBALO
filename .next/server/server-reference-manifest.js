f2abbfdafd892ba9f77714c9f3fbf2bd690ebca9
Name1 <nuxotetotmailsvoboden@gmail.com>
2026-07-31 16:32:55 +0000

    feat: full demo and promo pages

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"FLrCLkLR0Cv034zh5j+/HlN3GiOEeZcmw+fG+ax/D2g=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"t8WB3BX6rxSwXMnvTYszWXjsxS87RqOQlBRCybTPVFU=\"\n}"
